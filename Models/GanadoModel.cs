f1ce3ec4df69612f5f5eef9c874532655bd6e0c8
CCDM25 <ccdm25@CCDM25.local>
2019-03-04 09:27:47 -0700

    Agregado el modelo

---
+++ Models/GanadoModel.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace s102parcial1ingweb.Models
+{
+    public class GanadoModel
+    {
+        public int Id { get; set; }
+        public string TipoAnimal { get; set; }
+        public string Nombre { get; set; }
+        public int Peso { get; set; }
+        public string Color { get; set; }
+
+    }
+}
